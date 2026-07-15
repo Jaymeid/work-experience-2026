5749693e14d30b36787fb2f4c4d4cc19976a5423
imogencapner <imogenrcapner@gmail.com>
2026-07-15 15:17:36 +0100

    account card sections

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,22 @@
-import { Main } from "../components";
+import { A } from "storybook/internal/components";
+import { AccountCard, CardTitle, Main } from "../components";
+import AccountCardSection from "./AccountCardSection";
 
 export function HomePage() {
-  return <Main aria-label="Student workspace"></Main>;
+  return(
+  <Main aria-label="Student workspace">
+ <AccountCard name = "Current Balance" balance="£11,900"> </AccountCard>
+
+   <h1>Good Afternoon, Name.</h1>
+    <h3>Thank you for banking with us!</h3>
+    
+     <div style={{ marginBottom: '16px' }}>
+       <AccountCard name = "Savings Amount" balance="£2,619"> </AccountCard>
+     </div>
+     
+     <AccountCard name = "Spending" balance="Average of £2,009 spent monthly."> </AccountCard>
+    <AccountCardSection />
+    </Main>
+)
 }
+
