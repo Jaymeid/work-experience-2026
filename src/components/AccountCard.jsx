e527c408002a31f52a3475f5ee9453e16203563f
Eviehunter <erhunter1@icloud.com>
2026-07-15 15:05:00 +0100

    homepage

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -22,6 +22,9 @@ const Wrapper = styled.article`
   border-radius: ${({ theme }) => theme.radii.md};
   padding: 1.2rem;
   box-shadow: ${({ theme }) => theme.shadow};
+  position: relative;
+  overflow: hidden;
+  border-radius: 16px;
 
   ${({ variant = "primary" }) => looks[variant]}
 `;
@@ -30,26 +33,47 @@ const TopLine = styled.div`
   display: flex;
   justify-content: space-between;
   gap: 1rem;
+
+  position: relative;
 `;
 
 const Balance = styled.strong`
-  align-self: end;
+  position: absolute;
+  right: 1.2rem;
+  bottom: 1.2rem;
+
   font-size: 2rem;
 `;
 
 const Muted = styled.span`
   opacity: 0.76;
+
+  position: relative;
 `;
 
 export function AccountCard({ name, number, balance, change, variant }) {
   return (
     <Wrapper variant={variant}>
-      <TopLine>
-        <strong>{name}</strong>
-        <Muted>•• {number}</Muted>
-      </TopLine>
-      <Balance>{balance}</Balance>
-      <Muted>{change}</Muted>
+  <img
+    src="https://img.magnific.com/free-vector/digital-money-transfer-technology-background_1017-17454.jpg?semt=ais_hybrid&w=740&q=80"
+    alt="Bank app background"
+    style={{
+      position: "absolute",
+      inset: 0,
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+      zIndex: 0,
+    }}
+  />
+    <TopLine>
+    <strong>{name}</strong>
+    <Muted>•• {number}</Muted>
+  </TopLine>
+
+  <Balance>{balance}</Balance>
+
+  <Muted>{change}</Muted>
     </Wrapper>
   );
 }
