c5adf2cf9573d35f0457a5e1b913149e9b783eed
Jaymeid <jamespawson64@gmail.com>
2026-07-17 13:27:43 +0100

    Add normalize

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import { getRoute } from "./routes";
 
+function normalizePath(pathname) {
+  const withoutIndex = pathname.replace(/\/index\.html$/, "");
+
+  const withoutTrailingSlash =
+    withoutIndex.length > 1 ? withoutIndex.replace(/\/$/, "") : withoutIndex;
+
+  return withoutTrailingSlash || "/";
+}
+
 export function App() {
-  const { Component } = getRoute(window.location.pathname);
+  const basePath = import.meta.env.BASE_URL.replace(/\/$/, "");
+
+  const relativePath = window.location.pathname.startsWith(basePath)
+    ? window.location.pathname.slice(basePath.length) || "/"
+    : window.location.pathname;
+
+  const pathname = normalizePath(relativePath);
+
+  const { Component } = getRoute(pathname);
 
   return <Component />;
 }
