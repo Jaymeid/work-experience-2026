3a17e1cea672f092a350bc35a909304961ad7e45
Jaymeid <jamespawson64@gmail.com>
2026-07-17 13:32:58 +0100

    Add example page fixes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, "index.html"),
+        example: resolve(__dirname, "example-page/index.html"),
+      },
+    },
+  },
 });
